e9a3dc621faebecfb919a556f3ed770bdb023d95
NickSturz <n.sturz04@gmail.com>
2020-08-30 23:48:57 -0300

    Remocao do crossorigin

--- zuppersBoot/src/main/java/com/zuppersBoot/api/controller/usuariosController.java
+++ zuppersBoot/src/main/java/com/zuppersBoot/api/controller/usuariosController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,7 +25,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.zuppersBoot.domain.model.Usuarios;
 import com.zuppersBoot.domain.repository.UsuariosRepository;
 
-@CrossOrigin("http://localhost/")
+
 @RestController
 @RequestMapping("/usuario")
 public class usuariosController {
